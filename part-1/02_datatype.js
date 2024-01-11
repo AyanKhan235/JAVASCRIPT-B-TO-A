085cac5630d6694a57f26e35339c2be9a0295479
Ayan_khan_235 <babukhan640b@gmail.com>
2024-01-11 13:14:57 +0530

    datatype

---
+++ part-1/02_datatype.js
@@ -0,0 +1,26 @@
+"use Strict";
+// alert(4+3) // we are using node not browser
+console.log(3+
+    4);  // code readability should be high
+
+console.log("Ayan khan"); // it good
+
+
+let name="Ayan"
+let age=20
+let state;
+let city=null;
+
+// number 
+// string 
+// bitint
+// boolean =true or false
+// symbol ==> unique
+
+
+console.log(typeof "ayan"); // data type id string
+console.log(typeof age);  // data type id number
+console.log(typeof undefined);  // data type id undefined
+console.log(typeof null); // data type id object
+
+
