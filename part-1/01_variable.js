cf93a606bebc63c0339b9fe3c3e0c906b7d5c14a
Ayan_khan_235 <babukhan640b@gmail.com>
2024-01-11 12:46:36 +0530

    investigation of variables

---
+++ part-1/01_variable.js
@@ -0,0 +1,25 @@
+const accountId=14453
+let accountEmail="ayankhan@gmail.com"
+var accountPassword=121212
+accountCity="udaipur"
+let accountState
+
+
+
+// accountId=2;
+// cant change const value ;
+// console.log(accountId);
+
+ accountEmail="babu@gmail.com"
+ accountPassword=34353
+accountCity="jaipur"
+/*
+
+ prefer not to use var 
+ becouse of issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
