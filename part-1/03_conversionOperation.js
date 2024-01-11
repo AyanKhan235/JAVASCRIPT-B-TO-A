e07f44892d52a17207dda6caaefaa068b4e546da
Ayan_khan_235 <babukhan640b@gmail.com>
2024-01-11 13:39:25 +0530

    conversion

---
+++ part-1/03_conversionOperation.js
@@ -0,0 +1,44 @@
+let score = 33
+
+// const {score}=req.body // taking from front-end
+// let valueInNumber=Number(score);
+// console.log(typeof (score));
+// console.log(valueInNumber);
+
+
+// if score ="33ad"
+// it can convert in number but value is NaN
+
+// score="33ab";
+// valueInNumber=Number(score);
+// console.log(typeof score);
+// console.log(valueInNumber);
+
+// output is 
+// number
+// number
+// 33
+// string
+// NaN
+
+//  "33"=> 33
+//  "33shs"=>NaN
+// true=> 1 && faslse=>0
+//undefined=> NaN
+// null=>0
+
+
+// let isLoggedIn="ayan"; // 1 //""
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1=>true
+// 0=>false
+//""=> false
+//"ayan"=>true
+
+
+let somenumber=33;
+let stringNumber=String(somenumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
