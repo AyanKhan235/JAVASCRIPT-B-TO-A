1d37765a4c3713e3a70ff0182ba8ea4977718087
Ayan_khan_235 <babukhan640b@gmail.com>
2024-02-27 12:47:42 +0530

    responsive working

--- Sportify-clone_with_js/script.js
+++ Sportify-clone_with_js/script.js
@@ -130,6 +130,9 @@ async function main(){
     document.querySelector('.hamburger').addEventListener('click', ()=>{
         document.querySelector('.left').style.left="0"
     })
+    document.querySelector(".close").addEventListener('click',()=>{
+        document.querySelector('.left').style.left="-100%"
+    })
 
  };
 
